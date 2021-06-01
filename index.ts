e723cfbd2a4415e9a4e1fec5be53a8c87029498f
Kaito Udagawa <umireon@gmail.com>
2021-06-01 14:46:08 +0900

    With exModuleInterop: true

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-import express = require('express')
-import ace = require('atlassian-connect-express')
+import express from 'express'
+import ace, { AddOn } from 'atlassian-connect-express'
 
 const app = express()
 const addon = ace(app)
