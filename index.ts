b97efa1106ee0ae715badb6fa2af93b8f161ba3e
Kaito Udagawa <umireon@gmail.com>
2021-06-01 23:53:36 +0900

    Update the branch

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import ace, { HostClient, ClientInfo, AddOn, AddOnFactory} from 'atlassian-connect-express'
+import ace, { HostClient, ClientInfo, AddOn, AddOnFactory } from 'atlassian-connect-express'
 
 const app = express()
 const addon = ace(app)
